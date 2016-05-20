8fefd9993972da7663538fc5ec2c482061ebb8b0
lijihong <670259582@qq.com>
2016-05-20 15:37:45 +0800

    first commit

---
+++ MainActivity/gen/com/yangjie/cn/mypangting/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.yangjie.cn.mypangting;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MainActivity/java/com/yangjie/cn/mypangting/MainActivity.java
@@ -0,0 +1,87 @@
+package com.yangjie.cn.mypangting;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.graphics.drawable.GradientDrawable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        //浮动按钮
+        FloatingActionButton fab= (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Snackbar",Snackbar.LENGTH_SHORT)
+                        .setAction(R.string.app_name,null).show();
+            }
+        });
+
+        //导航
+        NavigationView navigationView= (NavigationView) findViewById(R.id.nv_view);
+        if(navigationView!=null){
+            setNavView(navigationView);
+        }
+
+    }
+
+    private void setNavView(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_About:
+                        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("DrawerLayout");
+                        builder.setMessage("打开了");
+                        builder.create().show();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
